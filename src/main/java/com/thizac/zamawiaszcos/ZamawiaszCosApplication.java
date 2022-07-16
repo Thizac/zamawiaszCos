52309ccf6eb27ddf202e884b1f7a0039e6dc3261
thizac <kuba6328224@gmail.com>
2022-07-16 10:49:56 +0200

    ZamawiaszCos - Spring init

---
+++ src/main/java/com/thizac/zamawiaszcos/ZamawiaszCosApplication.java
@@ -0,0 +1,13 @@
+package com.thizac.zamawiaszcos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZamawiaszCosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZamawiaszCosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thizac/zamawiaszcos/ZamawiaszCosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thizac.zamawiaszcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamawiaszCosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
